5c3bb99ab3c9775a9d89aede749d26eed429de04
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2024-01-10 16:30:43 -0300

    add generateVerificationToken and verifyToken tests

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,15 +1,19 @@
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { ConfirmEmailDto } from './dto/confirm-email.dto';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let jwtService: JwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [AuthService, JwtService],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    jwtService = module.get<JwtService>(JwtService);
   });
 
   it('should be defined', () => {
@@ -41,6 +45,37 @@ describe('AuthService', () => {
 
       const result = await service.compareHash('wrongPassword', hashedPassword);
       expect(result).toBe(false);
+    });
+  });
+
+  describe('generateVerificationToken', () => {
+    it('should generate a verification token', async () => {
+      const id = { id: '123' };
+      const expectedToken = 'token';
+      jest.spyOn(jwtService, 'signAsync').mockResolvedValue(expectedToken);
+
+      const result = await service.generateVerificationToken(id);
+
+      expect(jwtService.signAsync).toHaveBeenCalledWith(id);
+      expect(result).toBe(expectedToken);
+    });
+  });
+
+  describe('verifyToken', () => {
+    it('should verify a token', async () => {
+      const token = 'token';
+      const expectedPayload: ConfirmEmailDto = {
+        id: '123',
+        iat: 123456789,
+        exp: 123456789,
+      };
+
+      jest.spyOn(jwtService, 'verifyAsync').mockResolvedValue(expectedPayload);
+
+      const result = await service.verifyToken(token);
+
+      expect(jwtService.verifyAsync).toHaveBeenCalledWith(token);
+      expect(result).toBe(expectedPayload);
     });
   });
 });
